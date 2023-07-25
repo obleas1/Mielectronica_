6c92bcd1f98ded0b0ee0d5f17e3767300df6c3cd
NORMAS TECNICAS ESTATALES AMBIENTALES CGCE <ntea.cgce.sma@edomex.gob.mx>
2023-07-25 00:44:50 -0600

    Add project files.

---
+++ MiElectronica/MiElectronica/DAL/MyAppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MiElectronica.Models;
+
+namespace MiElectronica.DAL
+{
+    public class MyAppDbContext : DbContext
+    {
+        public MyAppDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ MiElectronica/MiElectronica/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MiElectronica.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
